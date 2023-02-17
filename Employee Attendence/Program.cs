f9e0285580f237e8aaafa793d102a04f13060d20
ammu72642668 <arundhamodepalli726426@gmail.com>
2023-02-17 15:21:35 +0530

    Employee calculation

--- Employee Attendence/Program.cs
+++ Employee Attendence/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Employee Attendence");
+            //Constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is abscent");
+            }
         }
     }
 }
